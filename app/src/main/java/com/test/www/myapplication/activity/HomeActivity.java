5f4a180c2e614c6f52f6d099dc97cd6cdae2e8fd
jam00 <jammarmalade@163.com>
2016-06-28 16:25:31 +0800

    核心优势界面

--- app/src/main/java/com/test/www/myapplication/activity/HomeActivity.java
+++ app/src/main/java/com/test/www/myapplication/activity/HomeActivity.java
@@ -58,10 +58,6 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if (!isTaskRoot()) {
-            finish();
-            return;
-        }
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
